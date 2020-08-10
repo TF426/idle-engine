076523d8f9cdcc3c7649c915438691e9510d1c09
kei-s16 <kei1216s@nifty.com>
2020-08-10 16:18:45 +0900

    コンポーネント分割試してみた

---
+++ client-src/client/components/mainmenu.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export default class MainMenu extends React.Component {
+  render() {
+    return (
+      <div >
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
